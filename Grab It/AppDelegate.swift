a8f247f71a79b06fd538efdc750d1994211d1552
Terry Johnson <giovanni7304@me.com>
2016-10-29 10:52:34 -0500

    Initial commit

--- Grab It/AppDelegate.swift
+++ Grab It/AppDelegate.swift
@@ -10,17 +10,70 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    
+    var item : NSStatusItem? = nil
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        item = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        item?.image = NSImage(named: "grabIT")
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Grab It!", action: #selector(AppDelegate.grabIt), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit!", action: #selector(AppDelegate.quit), keyEquivalent: ""))
+        
+        item?.menu = menu
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func printPasteBoard() {
+        if let items = NSPasteboard.general().pasteboardItems {
+            for item in items {
+                for type in item.types {
+                    if type == "public.html" || type == "public.utf8-plain-text" {
+                        let urlString = item.string(forType: type)
+                        print(urlString)
+                    }
+                }
+            }
+        }
+    }
+    
+    func grabIt() {
+        if let items = NSPasteboard.general().pasteboardItems {
+            for item in items {
+                for type in item.types {
+                    if type == "public.utf8-plain-text" {
+                        if let url = item.string(forType: type) {
+                            NSPasteboard.general().clearContents()
+                            
+                            var actualURL = ""
+                            
+                            if url.hasPrefix("http://") || url.hasPrefix("https://") {
+                                actualURL = url
+                            } else {
+                                actualURL = "http://\(url)"
+                            }
+                            
+                            NSPasteboard.general().setString("<a href=\(actualURL)\">\(url)</a>", forType: "public.html")
+                            NSPasteboard.general().setString(url, forType: "public.utf8-plain-text")
+                        }
+                    }
+                }
+            }
+        }
+        printPasteBoard()
+    }
+    
+    func quit() {
+        print("Quit Now")
+        NSApplication.shared().terminate(self)
+    }
+    
 }
 
---
+++ Grab It/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  Grab It
+//
+//  Created by Terry Johnson on 10/29/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import Foundation
