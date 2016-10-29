a6354c4758fbe1b21f8defef2f82f99110f48ba5
Terry Johnson <giovanni7304@me.com>
2016-10-28 19:48:11 -0500

    Initial Commit

---
+++ Grab It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Grab It
+//
+//  Created by Terry Johnson on 10/28/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
